64ab336e370f73a7f412d323b0f07588a6255161
Jenn Robbins <Jennrobbins3@gmail.com>
2022-02-23 10:36:47 -0500

    commit tree cca3a9ba42071d99d4c263c8b1bb42f6f701877e

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+
+const totalBatteries = batteryBatches.reduce(function(previousBatteries, currentValue) {
+    return currentValue + previousBatteries
+})
+
+    console.log(sumWithItitial);
+
+
+
+
+
+
+
